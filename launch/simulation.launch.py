1d1e2a7ee759f5330ff76679511eb6565ed5f469
Trent Shumay <trent@iotdesignshop.com>
2023-09-13 16:31:27 -0700

    Basic structure for simulation in place

---
+++ launch/simulation.launch.py
@@ -0,0 +1,48 @@
+from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    descr_path = get_package_share_path('dexhand_description')
+    default_model_path = descr_path / 'urdf/dexhand.xacro'
+    rviz_path = descr_path / 'rviz/urdf.rviz'
+
+    urdf = ParameterValue(Command(['xacro ', str(default_model_path)]),
+                                     value_type=str)
+
+    
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': urdf}]),
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher'),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', str(rviz_path)]),
+        #Node(
+        #    package='dexhand_gesture_controller',
+        #    executable='state_publisher',
+        #    name='state_publisher',
+        #    output='screen'),
+        
+    ])
--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
+
 
 package_name = 'dexhand_gesture_controller'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+  
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Trent Shumay',
+    maintainer='    Trent Shumay',
     maintainer_email='trent@iotdesignshop.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='A high-level semantic controller for the DexHand',
+    license='CC BY-NC-SA 4.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'state_publisher = dexhand_gesture_controller.state_publisher:main'
         ],
     },
 )
